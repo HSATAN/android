3e4468c430b33b1902970ca6e6b434ac88f301e7
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-09-05 20:47:07 +0800

    添加rxjava

--- app/src/main/java/com/example/edison/newworld/ItemActivity.java
+++ app/src/main/java/com/example/edison/newworld/ItemActivity.java
@@ -2,16 +2,27 @@ package com.example.edison.newworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by huangkaijie on 2017/9/5.
  */
 
 public class ItemActivity extends Activity {
+    @BindView(R.id.rv_text_list)
+    RecyclerView rvTextList;
+
 
     @Override
-    public void onCreate(Bundle save){
+    public void onCreate(Bundle save) {
         super.onCreate(save);
-        setContentView(R.layout.item_list);
+        setContentView(R.layout.item_main_list);
+        ButterKnife.bind(this);
+        rvTextList.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        rvTextList.setAdapter(new TextListAdapter(this));
     }
 }
