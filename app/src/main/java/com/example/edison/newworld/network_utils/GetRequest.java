9f7ad74101a4c300be9846e5e4bfbf49d1df1344
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-09-06 14:22:55 +0800

    添加retrofit

---
+++ app/src/main/java/com/example/edison/newworld/network_utils/GetRequest.java
@@ -0,0 +1,10 @@
+package com.example.edison.newworld.network_utils;
+
+import android.app.Activity;
+
+/**
+ * Created by huangkaijie on 2017/9/6.
+ */
+
+public class GetRequest extends Activity {
+}
---
+++ app/src/main/java/com/example/edison/newworld/network_utils/GetRequest_Interface.java
@@ -0,0 +1,8 @@
+package com.example.edison.newworld.network_utils;
+
+/**
+ * Created by huangkaijie on 2017/9/6.
+ */
+
+public interface GetRequest_Interface {
+}
